8e39b70eb356d8487fc070b476fdaf3951c806c7
Vengateshwari285 <vengateshwari2004@gmail.com>
2025-02-03 12:39:51 +0530

    server configure

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -22,22 +22,7 @@ exports.getSingleProduct = async (req, res) =>{
     }
 }
 
-//  exports.addNewProduct = async(req, res) =>{
-//     try{
-//         const product = await Product.findOne({proId: req.body.proId})
-//        if(!product){
-//             const addedProduct = await Product.create(req.body)
-//              res.json(addedProduct)
-//          }
-//          else{
-//              res.json({msg: 'Product already exixts'})
-//          }
-//      }
-//   catch(err){
-//         res.status(500).json({msg:err.message})
-//     }
-//  }
-
+/
 // exports.updateProduct = async (req, res) =>{
 //    try{
 //          const product = req.body
